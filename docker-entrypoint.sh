3a2f2592bb43a11aa1823776b476714ea1521395
Arijit Dey <arijit.dey18@icloud.com>
2019-05-23 15:54:38 -0700

    Updating docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-if ! [ -f backup-wp-cli-cron ]
-then
+echo "Creating cron entry to start backup at: $BACKUP_TIME"
 
-  echo "Creating cron entry to start backup at: $BACKUP_TIME"
-  # Note: Must use tabs with indented 'here' scripts.
-  cat <<-EOF >> backup-wp-cli-cron
+# Note: Must use tabs with indented 'here' scripts.
+cat <<-EOF >> backup-wp-cli-cron
 WORDPRESS_DB_HOST=$WORDPRESS_DB_HOST
 WORDPRESS_DB_USER=$WORDPRESS_DB_USER
 WORDPRESS_DB_NAME=$WORDPRESS_DB_NAME
 WORDPRESS_DB_PASSWORD=$WORDPRESS_DB_PASSWORD
 EOF
 
-  echo "$BACKUP_TIME backup " >> backup-wp-cli-cron
+echo "$BACKUP_TIME backup " >> backup-wp-cli-cron
 
-  crontab backup-wp-cli-cron
-fi
+crontab backup-wp-cli-cron
 
 echo "Current crontab:"
 crontab -l
